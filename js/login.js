dd9b3f5b8cab402ed2f8ba8598df5600c69d3815
micheldiazjap <michel_2692@hotmail.com>
2022-11-23 18:32:07 -0300

    desafates

--- js/login.js
+++ js/login.js
@@ -32,3 +32,15 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     })
 });
+function handleCredentialResponse(response) {
+     // decodeJwtResponse() is a custom function defined by you
+     // to decode the credential response.
+     const responsePayload = decodeJwtResponse(response.credential);
+
+     console.log("ID: " + responsePayload.sub);
+     console.log('Full Name: ' + responsePayload.name);
+     console.log('Given Name: ' + responsePayload.given_name);
+     console.log('Family Name: ' + responsePayload.family_name);
+     console.log("Image URL: " + responsePayload.picture);
+     console.log("Email: " + responsePayload.email);
+  }
